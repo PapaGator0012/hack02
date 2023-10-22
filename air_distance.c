785b00dc2a8e28be147f41b99a503a9f45ce8a2c
Hassam <144311429+PapaGator0012@users.noreply.github.com>
2023-10-22 12:35:28 -0700

    input and output statemtns

--- air_distance.c
+++ air_distance.c
@@ -4,14 +4,25 @@
 int main()
 {
 
-float latOrigin,longOrigin,latDetination,longDestination;
+float latOrigin,longOrigin,latDestination,longDestination;
 float distance,delta;
 const float pi=3.14;  // constant > holds value for pi
 const float radius= 6371.0 // constant > holds value of radius of earth
 
 // input values from users
+// the input values are assumed as degrees
 
+printf("enter values for origin latitude");
+scanf("%f",&latOrigin);
+printf("enter value for origin longitude");
+scanf("%f",&longOrigin);
+printf("\n");
+printf("enter values for destination latitude");
+scanf("%f",&latDestination);
+printf("enter values for destination longitude");
+scanf("%f",&longDestination);
 
+// using a mathematical formula radian=pi/180
 
 
 
