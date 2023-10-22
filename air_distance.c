72f9de9eb9473eada0ba20a387d15ed38ef76a6b
Hassam <144311429+PapaGator0012@users.noreply.github.com>
2023-10-22 12:26:04 -0700

    declaring variables and constants

---
+++ air_distance.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<math.h>
+#include < stdlib.h>
+int main()
+{
+
+float latOrigin,longOrigin,latDetination,longDestination;
+float distance,delta;
+const float pi=3.14;  // constant > holds value for pi
+const float radius= 6371.0 // constant > holds value of radius of earth
+
+// input values from users
+
+
+
+
+
+
+
+
+    
+
+
+    return 0;
+}
